b2d84c28976ff5103f426b5eb8cefc6ef39d55a3
KimSeungDeok <tmdejr1117@gmail.com>
2022-02-16 18:55:23 +0900

    빌드 및 렌더링 완료

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: path.join(__dirname, 'src', 'index.js'),
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+devServer: {
+    static: {
+        directory: path.join(__dirname,  'dist'),
+      },
+    hot: false,
+    compress: true,
+    port: 3001, // 원하는 포트 번호
+  },
+};
